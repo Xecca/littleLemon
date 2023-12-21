98b7baefa6805d576f03cec1fb03fd86097413b1
Andrei Ponomarenko <ensimgen@gmail.com>
2023-12-21 16:27:56 +0300

    Create a project.

---
+++ LittleLemonApp/LittleLemonApp/LittleLemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by RedOrcDron on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
